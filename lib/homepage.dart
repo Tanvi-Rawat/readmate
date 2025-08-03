837e37e3490e3cd08862739a6a93eb764d666a02
Tanvi0Rawat <10143tanvirawat@gmail.com>
2025-08-03 12:12:19 +0530

    Added bottom nav bar and page routing

---
+++ lib/homepage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:readmate/pages/page1.dart';
+import 'package:readmate/pages/page2.dart';
+import 'package:readmate/pages/page3.dart';
+
+
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  //when we want to build a screen that can change (update) while the app is running.
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+
+  }
+
+  final List<Widget> _pages = [
+    Userhome(),
+    UserLib(),
+    UserProfile(),
+
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Library'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/pages/page1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Userhome extends StatelessWidget {
+  const Userhome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "HomePage",
+        style: TextStyle(fontSize: 40),
+
+      ),
+    );
+  }
+}
+
