dfb54a358ba2ed1f4f03efd00215448f08f8b1a4
Tanvi0Rawat <10143tanvirawat@gmail.com>
2025-08-04 22:21:10 +0530

    Added AddPage

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../add.dart';
+
+// The class for the Userhome widget.
 class Userhome extends StatelessWidget {
   const Userhome({super.key});
 
+  // The build method is correctly placed inside the class.
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "HomePage",
-        style: TextStyle(fontSize: 40),
-
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(20),
+          ),
+          child: const Text('add', style: TextStyle(fontSize: 20)),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddPage()),
+            );
+          },
+        ),
       ),
     );
   }
